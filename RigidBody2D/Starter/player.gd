f618ecafbea85eec4207fae77ead53d036c4bc07
hexblit <hexblit@gmail.com>
2023-08-28 11:58:09 -0600

    Updated Starter project to align needed starting setup

--- RigidBody2D/Starter/player.gd
+++ RigidBody2D/Starter/player.gd
@@ -1,14 +1,23 @@
 extends RigidBody2D
  
+
+@onready var thruster = $Fire07
+@onready var ship = $Sprite2D
+@onready var explosionAnimation = $ExplosionAnimation
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
+	if Input.is_action_pressed("ui_up"):
+		thruster.show()
+	else:
+		thruster.hide()
+	
+
+func _physics_process(delta):
 	pass
 
-  
- 
  
